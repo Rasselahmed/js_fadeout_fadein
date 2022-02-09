0edc99cdf92e110d991db937fff42550700bf4fe
Rassel Ahmed <94746949+Rasselahmed@users.noreply.github.com>
2022-02-09 12:31:33 +0600

    Add files via upload
    upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//fadeout
+//fadein
+//fadetoggle
+//fadeto
+
+
+$(document).ready(function () {
+    $('#fadeout').click(function () {
+        $('.box').fadeOut(3000);
+    });
+    $('#fadein').click(function () {
+        $('.box').fadeIn(3000);
+    });
+
+
+
+});
